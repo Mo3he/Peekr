dee36348424f55fc59799426adf3f3f006536dba
Weston Blieden <mblieden@gmail.com>
2026-04-30 22:02:37 +0200

    Add Xcode Cloud post-clone script to generate xcodeproj via XcodeGen

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+# Install XcodeGen via Homebrew (Homebrew is available on Xcode Cloud agents)
+brew install xcodegen
+
+# Generate Peekr.xcodeproj from project.yml
+xcodegen generate --spec "$CI_PRIMARY_REPOSITORY_PATH/project.yml" --project "$CI_PRIMARY_REPOSITORY_PATH"
